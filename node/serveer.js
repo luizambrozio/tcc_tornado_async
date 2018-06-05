34dc9964dcb675e94b599b026905f75d7195b285
Luiz Ambrozio <oliveira.ambrozio@gmail.com>
2018-06-04 22:01:55 -0300

    server node /pedidos e /produtos

--- node/serveer.js
+++ node/serveer.js
@@ -1,29 +1,14 @@
 var express = require('express');
 const delay = require('delay');
+var bodyParser = require('body-parser');
 
 var app = express();
 
-app.get('/2s', function (req, res) {
-  delay(2000, 'a result')
-    .then(result => {
-        console.log('2000');
-        res.send('val');
-    });
 
-});
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded()); // for parsing application/x-www-form-urlencoded
 
-app.get('/5s', function (req, res) {
-  delay(5000, 'a result')
-    .then(result => {
-        console.log('5000');
-        res.send('val');
-    });
 
-});
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
 // {
 // Avelã = 3s
 // Banoffee = 2.5s
@@ -34,3 +19,44 @@ app.listen(3000, function () {
 // refri = 1s
 // sanduiche = 10s
 // }
+
+var produtos = [
+  {key:"Avelã", value: 3000},
+  {key:"Banoffee", value:2500},
+  {key:"Brigadeiro", value:2000},
+  {key:"tradicional", value:1500},
+  {key:"cha", value:2000},
+  {key:"agua", value:1000},
+  {key:"refrigerante", value:1000},
+  {key:"sanduiche", value:10000}
+]; // create an empty array
+
+app.get('/produtos', function (req, res) {
+  res.send(produtos)
+});
+
+
+app.post('/pedido', function (req, res) {
+  let pedidos = req.body.pedido
+  let time = 0
+
+  pedidos.forEach(element => {
+    produtos.forEach(e => {
+      if (e.key === element) {
+          if (e.value > time) {
+            time =e.value
+          }        
+      }
+    })
+  });
+
+  delay(time, 'a result')
+    .then(result => {
+      res.json(pedidos);
+    }); 
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
